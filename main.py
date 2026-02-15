4a54919c00d4f175dd95746dc01fe55d0661e11c
itsmenneka <nnekaxx@gmail.com>
2026-02-15 10:58:26 -0400

    final

--- main.py
+++ main.py
@@ -60,25 +60,116 @@ with open("config.json.gitignore", "r") as config_file:
     if not data["has-set-up"]:
         set_up()
 
-path = data["source-directory"]
 
-file_name = os.listdir(path) # names all the files in that path
+def organize():
+    path = data["source-directory"]
 
-folder_names = list(data["rules"].values())
+    file_name = os.listdir(path) # names all the files in that path
 
-# creates the folders if they don't exist
-for loop in range(len(folder_names)):
-    if not os.path.exists(path + "/" + folder_names[loop]):
-        os.makedirs((path + "/" + folder_names[loop]))
+    folder_names = list(data["rules"].values())
 
-# moves the files into correct folders
-for file in file_name:
-    for rule in data["rules"]:
-         if rule in file and file not in data["ignore-files"]:
-             folder_name = data["rules"][rule]
-             if not os.path.exists(path + "/" + folder_name + "/" + file):
-                 print(path + "/" + file, path + "/" + folder_name + "/" + file)
-                 shutil.move(path + "/" + file, path + "/" + folder_name + "/" + file)
-                 e=3
-             else:
-                 print("File already exists in destination folder.")
+    # creates the folders if they don't exist
+    for loop in range(len(folder_names)):
+        if not os.path.exists(path + "/" + folder_names[loop]):
+            os.makedirs((path + "/" + folder_names[loop]))
+            print("Created folder: " + folder_names[loop])
+
+    # moves the files into correct folders
+    for file in file_name:
+        for rule in data["rules"]:
+            if rule in file and file not in data["ignore-files"]:
+                folder_name = data["rules"][rule]
+                if not os.path.exists(path + "/" + folder_name + "/" + file):
+                    shutil.move(path + "/" + file, path + "/" + folder_name + "/" + file)
+                else:
+                    print("File already exists in destination folder.")
+
+print("Commands:")
+print("organize - organize files")
+print("setup - resetup the program and prefrences")
+print("srcfolder - change the source folder")
+print("destfolder - change the destination folder")
+print("addrule - add a new rule for sorting files")
+print("currentrules - view current rules for sorting files")
+print("addignore - add a file to the ignore list")
+print("currentignore - view current ignore list")
+print("removerule - remove a rule from the sorting rules")
+print("removeignore - remove a file from the ignore list")
+
+while True:
+    user_input = input()
+    user_input = user_input.lower()
+    if user_input == "organize":
+        organize()
+    elif user_input == "setup":
+        set_up()
+    elif user_input == "srcfolder":
+        print("Enter the new source folder path:")
+        new_src = input()
+        data["source-directory"] = new_src
+        with open("config.json.gitignore", "w") as config_file:
+            json.dump(data, config_file, indent=4)
+    elif user_input == "destfolder":
+        print("Enter the new destination folder path:")
+        new_dest = input()
+        data["destination-directory"] = new_dest
+        with open("config.json.gitignore", "w") as config_file:
+            json.dump(data, config_file, indent=4)
+    elif user_input == "addrule":
+        print("Eg. .png Images")
+        print("type done when done")
+        done = False
+        while not done:
+            user_input = input()
+            if user_input == "done":
+                done = True
+                print("Rule(s) added.")
+            else:
+                user_input = user_input.split()
+                folder_name = ""
+                for i in range(1, len(user_input)):
+                    folder_name += user_input[i] + " "
+                folder_name = folder_name.strip()
+                data["rules"][user_input[0]] = folder_name
+                with open("config.json.gitignore", "w") as config_file:
+                    json.dump(data, config_file, indent=4)
+    elif user_input == "currentrules":
+        print("Current sorting rules:")
+        for rule, folder in data["rules"].items():
+            print(f"{rule} -> {folder}")
+    elif user_input == "addignore":
+        print("Enter the file name to ignore:")
+        print("type done when done")
+        done = False
+        while not done:
+            ignore_file = input()
+            if ignore_file == "done":
+                done = True
+            else:
+                data["ignore-files"].append(ignore_file)
+                with open("config.json.gitignore", "w") as config_file:
+                    json.dump(data, config_file, indent=4)
+    elif user_input == "currentignore":
+        print("Current ignore list:")
+        for ignore in data["ignore-files"]:
+            print(ignore)
+    elif user_input == "removerule":
+        print("Enter the rule pattern to remove (e.g., .pdf)")
+        rule_to_remove = input()
+        if rule_to_remove in data["rules"]:
+            del data["rules"][rule_to_remove]
+            with open("config.json.gitignore", "w") as config_file:
+                json.dump(data, config_file, indent=4)
+            print(f"Rule {rule_to_remove} removed.")
+        else:
+            print("Rule not found.")
+    elif user_input == "removeignore":
+        print("Enter the file name to remove from ignore list:")
+        ignore_to_remove = input()
+        if ignore_to_remove in data["ignore-files"]:
+            data["ignore-files"].remove(ignore_to_remove)
+            with open("config.json.gitignore", "w") as config_file:
+                json.dump(data, config_file, indent=4)
+            print(f"File {ignore_to_remove} removed from ignore list.")
+        else:
+            print("File not found in ignore list.")
